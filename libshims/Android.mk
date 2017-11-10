36b1bd3be77586650bd95176d045563cf6b3b361
danile71 <danile71@gmail.com>
2017-11-10 12:40:19 +0300

    libshims: add libizat_core shim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,17 +1,33 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+<<<<<<< HEAD
 LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils
+=======
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils
+
+>>>>>>> f8c72c3... libshims: add libizat_core shim
 LOCAL_SRC_FILES := \
     gps_shim.cpp
 
 LOCAL_MODULE := libshim_gps
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+<<<<<<< HEAD
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils
+=======
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils
+
+>>>>>>> f8c72c3... libshims: add libizat_core shim
 LOCAL_SRC_FILES := \
     secril_shim.cpp
 
@@ -19,6 +35,7 @@ LOCAL_MODULE := libshim_secril
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
+<<<<<<< HEAD
 
 include $(CLEAR_VARS)
 
@@ -49,3 +66,5 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
+=======
+>>>>>>> f8c72c3... libshims: add libizat_core shim
